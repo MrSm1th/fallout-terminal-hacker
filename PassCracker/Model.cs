f412b0c4de35fbe85e53a1514608a2ff89c61ac4
MrSm1th <villahov@gmail.com>
2016-02-18 00:05:14 +0300

    v1.0

---
+++ PassCracker/Model.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassCracker
+{
+    public class PasswordEntry
+    {
+        public string Text { get; set; }
+        public int[] Matches { get; set; }
+
+        public int Rank
+        {
+            get { return Matches.Where(m => m > 0).Count(); }
+        }
+
+        public PasswordEntry(string text): this(text, new int[0]) { }
+        public PasswordEntry(string text, int[] matches)
+        {
+            Text = text;
+            Matches = matches;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0:00} {1} [{2}]", Rank, Text, string.Join(" ", Matches));
+        }
+    }
+
+    public class Model
+    {
+        private List<string> _textEntries;
+        private Dictionary<string, int[]> _entries;
+        public IEnumerable<PasswordEntry> Entries
+        {
+            get
+            {
+                return _entries
+                    .Select(e => new PasswordEntry(e.Key, e.Value))
+                    .OrderByDescending(e => e.Rank)
+                    .ThenBy(e => e.Text);
+            }
+        }
+
+        public Model()
+        {
+            _entries = new Dictionary<string, int[]>();
+            _textEntries = new List<string>();
+        }
+
+        public void Add(string entry)
+        {
+            if (!_entries.ContainsKey(entry))
+            {
+                _entries.Add(entry, null);
+                _textEntries.Add(entry);
+                UpdateEntriesMatches();
+            }
+        }
+
+        public void Remove(string entry)
+        {
+            _entries.Remove(entry);
+            _textEntries.Remove(entry);
+            UpdateEntriesMatches();
+        }
+
+        public void Clear()
+        {
+            _textEntries.Clear();
+            _entries.Clear();
+        }
+
+        public void Filter(string entry, int matches)
+        {
+            if (!_entries.ContainsKey(entry)) return;
+
+            _entries.Remove(entry);
+            foreach (var e in _entries.Keys.ToArray())
+            {
+                if (Compare(e, entry) != matches)
+                    _entries.Remove(e);
+            }
+            UpdateEntriesMatches();
+        }
+
+        public void ClearFilter()
+        {
+            var tmpCopy = _textEntries.ToList();
+            _textEntries.Clear();
+            _entries.Clear();
+            tmpCopy.ForEach(e => Add(e));
+        }
+
+
+        private void UpdateEntriesMatches()
+        {
+            foreach (var e in _entries.Keys.ToArray())
+            {
+                _entries[e] = CompareToOthers(e);
+            }
+        }
+
+        private int[] CompareToOthers(string variant)
+        {
+            var matches = new int[variant.Length];
+            foreach (var v in _entries.Keys)
+            {
+                if (v.Equals(variant)) continue;
+
+                var match = Compare(v, variant);
+                matches[match]++;
+            }
+
+            return matches.Skip(1).ToArray();
+        }
+
+        private int Compare(string s1, string s2)
+        {
+            int n = 0;
+            for (int i = 0; i < s1.Length && i < s2.Length; i++)
+            {
+                if (s1[i] == s2[i]) n++;
+            }
+
+            return n;
+        }
+    }
+}
---
+++ PassCracker/ViewModel.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PassCracker
+{
+    public class MainViewModel: INotifyPropertyChanged
+    {
+        #region Fields
+        
+        private Model Model;
+
+        #endregion
+
+
+        #region Properties
+
+        public IEnumerable<PasswordEntry> Passwords
+        {
+            get
+            {
+                var e = Model.Entries;
+                TotalEntries = e.Count();
+                OnPropertyChanged("TotalEntries");
+                return e;
+            }
+        }
+        public int TotalEntries { get; set; }
+
+        private PasswordEntry _selectedEntry;
+        public PasswordEntry SelectedEntry
+        {
+            get { return _selectedEntry; }
+            set
+            {
+                if (_selectedEntry != value)
+                {
+                    _selectedEntry = value;
+                    OnPropertyChanged("SelectedEntry");
+                }
+            }
+        }
+
+        private string _tbPassword;
+        public string TbPassword
+        {
+            get { return _tbPassword; }
+            set
+            {
+                if (_tbPassword != value)
+                {
+                    _tbPassword = value;
+                    OnPropertyChanged("TbPassword");
+                }
+            }
+        }
+
+        private int _filterMatches;
+        public int FilterMatches
+        {
+            get { return _filterMatches; }
+            set { _filterMatches = value; }
+        }
+
+
+        #endregion
+
+
+        #region Commands
+
+        public ICommand Add { get; set; }
+        public ICommand Remove { get; set; }
+        public ICommand RemoveAll { get; set; }
+        public ICommand Filter { get; set; }
+        public ICommand Clear { get; set; }
+
+        #endregion
+
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+
+
+        public MainViewModel()
+        {
+            Model = new PassCracker.Model();
+
+            Add = new Command(AddEntry);
+            Remove = new Command<IEnumerable>(RemoveEntries);
+            RemoveAll = new Command(RemoveAllEntries);
+            Filter = new Command(FilterEntries, () => SelectedEntry != null);
+            Clear = new Command(ClearFilter);
+
+            //AddTmpData();
+        }
+
+        //private void AddTmpData()
+        //{
+        //    //var tmp = "observation,interesting,temperature,combination,desperately,examination,infestation,immediately,influential,eliminating,undrinkable,explanation,destructive,unimportant,radioactive,agriculture,recognition,manufacture";
+        //    //var tmp = "permanently beckoningly hospitality descendants opportunity persecution examination destructive recognition devastation combination personality reluctantly eliminating destruction reprimanded observation partitioned";
+        //    var tmp = "mushroom resemble resulted distance leverage describe absolute decorate destroys assembly generate obsessed";
+        //    foreach (var i in tmp.Split(' '))
+        //    {
+        //        TbPassword = i;
+        //        AddEntry();
+        //    }
+        //}
+
+
+
+        private void AddEntry()
+        {
+            if (string.IsNullOrWhiteSpace(TbPassword)) return;
+
+            Model.Add(TbPassword);
+            TbPassword = string.Empty;
+
+            OnPropertyChanged("Passwords");
+        }
+
+        private void RemoveEntries(IEnumerable items)
+        {
+            if (items == null) return;
+
+            new List<PasswordEntry>(items.Cast<PasswordEntry>())
+                .ForEach(entry => Model.Remove(entry.Text));
+
+            OnPropertyChanged("Passwords");
+        }
+
+        private void RemoveAllEntries()
+        {
+            Model.Clear();
+            OnPropertyChanged("Passwords");
+        }
+
+        private void FilterEntries()
+        {
+            string matchEntry = SelectedEntry.Text;
+            int filterMatches = FilterMatches;
+
+            Model.Filter(matchEntry, filterMatches);
+
+            OnPropertyChanged("Passwords");
+        }
+
+        private void ClearFilter()
+        {
+            Model.ClearFilter();
+
+            OnPropertyChanged("Passwords");
+        }
+    }
+
+
+    public class Command : ICommand
+    {
+        private Action _action;
+        private Func<bool> _canExecute;
+
+        public Command(Action action) : this(action, true) { }
+        public Command(Action action, bool canExecute) : this(action, () => true) { }
+        public Command(Action action, Func<bool> canExecute)
+        {
+            if (action == null) throw new ArgumentNullException("action");
+            if (canExecute == null) throw new ArgumentNullException("canExecute");
+
+            _action = action;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+    }
+
+    public class Command<T> : ICommand
+    {
+        private Action<T> _action;
+        private Func<bool> _canExecute;
+
+        public Command(Action<T> action) : this(action, true) { }
+        public Command(Action<T> action, bool canExecute): this(action, () => true) { }
+        public Command(Action<T> action, Func<bool> canExecute)
+        {
+            _action = action;
+            _canExecute = canExecute;
+        }
+
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter is T)
+                _action((T)parameter);
+        }
+    }
+}
